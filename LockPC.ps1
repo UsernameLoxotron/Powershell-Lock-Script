5831ba8b82816cb0ffb5b85a44070d405a245bec
UsernameLoxotron <68998003+UsernameLoxotron@users.noreply.github.com>
2026-02-27 17:38:54 -0500

    ASCII art added

--- LockPC.ps1
+++ LockPC.ps1
@@ -1,6 +1,32 @@
-# Stop browsers, or any other software
-$soft = @("chrome", "msedge", "firefox", "brave", "opera")
-Stop-Process -Name $soft -Force -ErrorAction SilentlyContinue
+# C# Windows BlockInput API
+$signature = @"
+[DllImport("user32.dll", CharSet=CharSet.Auto, ExactSpelling=true)]
+public static extern bool BlockInput(bool fBlockIt);
+"@
+$BlockInput = Add-Type -MemberDefinition $signature -Name "InputBlocker" -Namespace "Win32" -PassThru
 
-# Lock PC
-rundll32.exe user32.dll,LockWorkStation
+# Text to display
+$asciiArt = @"
+                                .oodMMMM
+                       .oodMMMMMMMMMMMMM
+           ..oodMMM  MMMMMMMMMMMMMMMMMMM
+     oodMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM
+     MMMMMMMMMMMMMM  MMMMMMMMMMM…and closing browsers in 5 seconds..." -ForegroundColor Yellow
+
+try {
+    # Disable keyboard and mouse
+    $BlockInput::BlockInput($true) | Out-Null
+
+    Start-Sleep -Seconds 5
+
+    # Stop browsers, or any other software
+    $soft = @("chrome", "msedge", "firefox", "brave", "opera")
+    Stop-Process -Name $soft -Force -ErrorAction SilentlyContinue
+
+    # Lock PC
+    rundll32.exe user32.dll,LockWorkStation
+}
+finally {
+    # Enable keyboard and mouse
+    $BlockInput::BlockInput($false) | Out-Null
+}
