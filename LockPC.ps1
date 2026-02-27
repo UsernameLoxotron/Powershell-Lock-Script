bf1b8727d45db7833e9ad26127b9d79c55c860ae
UsernameLoxotron <68998003+UsernameLoxotron@users.noreply.github.com>
2026-02-27 17:45:35 -0500

    Admin check

--- LockPC.ps1
+++ LockPC.ps1
@@ -1,17 +1,34 @@
+# Self-elevate to Administrator
+$isAdmin = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+if (-not $isAdmin) {
+    Start-Process powershell.exe "-ExecutionPolicy Bypass -WindowStyle Maximized -File `"$PSCommandPath`"" -Verb RunAs
+    exit
+}
+
+# Full Screen
+$wshell = New-Object -ComObject wscript.shell
+$wshell.SendKeys('{F11}')
+Start-Sleep -Milliseconds 250
+
 # C# Windows BlockInput API
 $signature = @"
 [DllImport("user32.dll", CharSet=CharSet.Auto, ExactSpelling=true)]
 public static extern bool BlockInput(bool fBlockIt);
 "@
-$BlockInput = Add-Type -MemberDefinition $signature -Name "InputBlocker" -Namespace "Win32" -PassThru
 
-# Text to display
+if (-not ("Win32.InputBlocker" -as [type])) {
+    $BlockInput = Add-Type -MemberDefinition $signature -Name "InputBlocker" -Namespace "Win32" -PassThru
+} else {
+    $BlockInput = [Win32.InputBlocker]
+}
+
+# 4. Text to display
 $asciiArt = @"
                                 .oodMMMM
                        .oodMMMMMMMMMMMMM
            ..oodMMM  MMMMMMMMMMMMMMMMMMM
      oodMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM
-     MMMMMMMMMMMMMM  MMMMMMMMMMM…M
+     MMMMMMMMMMMMMM  MMMMMMMMMMM...M
      MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM
      MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM
      MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM
@@ -29,18 +46,14 @@ $asciiArt = @"
 
 Clear-Host
 Write-Host $asciiArt -ForegroundColor Cyan
-Write-Host "`nLocking workstation and closing browsers in 5 seconds..." -ForegroundColor Yellow
 
 try {
     # Disable keyboard and mouse
     $BlockInput::BlockInput($true) | Out-Null
-
-    Start-Sleep -Seconds 5
-
+    Start-Sleep -Seconds 2
     # Stop browsers, or any other software
     $soft = @("chrome", "msedge", "firefox", "brave", "opera")
     Stop-Process -Name $soft -Force -ErrorAction SilentlyContinue
-
     # Lock PC
     rundll32.exe user32.dll,LockWorkStation
 }
